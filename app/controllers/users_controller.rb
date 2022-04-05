6daf382f8d03abbda0a8bfc175370e84b706df90
ShogoHada <hs109105@gmail.com>
2022-04-05 22:02:55 +0900

    fix　ログインしなくても詳細ページ入れていた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
+  before_action :authenticate_user!
 
   def index
   end
 
   def show
+    flash[:notice] = "ログイン済ユーザーのみ記事の詳細を確認できます" unless user_signed_in?
     @item = @user.used_words.page(params[:page]).per(20)
   end
 
